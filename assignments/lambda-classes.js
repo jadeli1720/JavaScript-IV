36a5fc78228e488a4427fd1b4c32c5dbfe667e41
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-06-13 14:06:30 -0600

    Finished and fixed problem with prototype-refractor. Started on lamba-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,94 @@
 // CODE here for your Lambda Classes
+
+class Person { //parent
+    constructor(attrs) {
+        this.name = attrs.name;
+        this.age = attrs.age;
+        this.location = attrs.location;
+    }
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}`
+    }
+}
+
+class Instructor extends Person { //child
+    constructor(instructAttrs) {
+        super(instructAttrs)
+        this.specialty = instructAttrs.specialty
+        this.favLanguage = instructAttrs.favLanguage
+        this.catchPhrase = instructAttrs.catchPhrase
+    }
+    demo(subject) {
+        return `Today we are learning about ${subject}.`
+    }
+
+    grade(student) {
+        return `${student.name} receives a perfect score on ${subject}`
+    }
+}
+
+class Student extends Person {
+    constructor(studAttrs) {
+        this.previousBackground = studAttrs.previousBackground
+        this.className = studAttrs.className
+        this.favSubjects = studAttrs.favSubjects
+    }
+}
+
+
+
+// const Austin = new ProjectManager({
+//     name: 'Austin',
+//     age: '23',
+//     gradClassName: 'WEB18',
+//     favInstructor: 'Josh knell',
+//     location: 'Somewhere',
+//     specialty: 'Java',
+//     favLanguage: 'Java',
+//     catchPhrase: ":eggplant:",
+// });
+
+// const fred = new Instructor({
+//   name: 'Fred',
+//   location: 'Bedrock',
+//   age: 37,
+//   favLanguage: 'JavaScript',
+//   specialty: 'Front-end',
+//   catchPhrase: `Don't forget the homies`
+// });
+
+// const dan = new Instructor({
+//     name: 'Dan',
+//     age: Infinity,
+//     location: 'Denver',
+//     specialty: 'Relentless Debugger',
+//     favLanguage: 'JavaScript, Python, Elm, ReasonML, SmallTalk, Haskell, C#, Java, Rust, Go, Ruby, Crystal, Elixir, Lua, and Julia', 
+//     catchPhrase: 'If you can do the thing, you can get paid to do the thing!'
+// });
+
+// const kevin = new Student({
+//     name: "Kevin",
+//     age: 28,
+//     location: "California",
+//     previousBackground: "Table Games Dealer",
+//     className: "WEB21",
+//     favSubjects: ["Html", "CSS", "JavaScript"]
+//   });
+
+//   const nisa = new Student({
+//     name: 'Nisa',
+//     age: 25,
+//     location: 'Ohio',
+//     previousBackground: 'Debt Collector',
+//     className: 'Web21',
+//     favSubjects: ['Html', 'CSS', 'JavaScript'],
+// });
+
+// const joscelyn = new Student({
+//     name: "Joscelyn",
+//     age: 29,
+//     location: "California",
+//     previousBackground: "English teacher",
+//     className: "WEB21",
+//     favSubjects: ["computer science", "philosophy", "English"]
+//   });
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -17,7 +17,7 @@ class GameObject {//parent
         this.dimensions = attr.dimensions;
     }
     destroy() {
-        return `${this.name} was removed from the game.`
+        return `${this.name} was removed from the game.`;
     }
 }
 
@@ -26,21 +26,21 @@ class GameObject {//parent
 class CharacterStats extends GameObject { //child of GameObject
     constructor(attr) {
         super(attr) //Makes things work (Super Sauce)
-        this.healthPoints = attr.healthPoints
+        this.healthPoints = attr.healthPoints;
     }
-    takeDamage() {
+    takeDamage () {
         return `${this.name} took damage`;
     }
 }
 // super() replaced :GameObject.call(this, attributes); Helps child 
 // to inherit properties from parent GameObject
 
-class Humanoid extends GameObject { //child of GameObject
+class Humanoid extends CharacterStats { // child of CharacterStats > grandchild of GameObject
     constructor(attr) {
         super(attr) //Super Sauce
-        this.team = attributes.team,
-        this.weapons = attributes.weapons,
-        this.language = attributes.language;
+        this.team = attr.team,
+        this.weapons = attr.weapons,
+        this.language = attr.language;
     }
     greet () {
         return `${this.name} offers a greeting in  ${this.language}`;
@@ -49,63 +49,64 @@ class Humanoid extends GameObject { //child of GameObject
 }
 
 
-// const mage = new Humanoid({
-//     createdAt: new Date(),
-//     dimensions: {
-//       length: 2,
-//       width: 1,
-//       height: 1,
-//     },
-//     healthPoints: 5,
-//     name: 'Bruce',
-//     team: 'Mage Guild',
-//     weapons: [
-//       'Staff of Shamalama',
-//     ],
-//     language: 'Common Tongue',
-//   });
+const mage = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 1,
+      height: 1,
+    },
+    healthPoints: 5,
+    name: 'Bruce',
+    team: 'Mage Guild',
+    weapons: [
+      'Staff of Shamalama',
+    ],
+    language: 'Common Tongue',
+  });
 
-//   const swordsman = new Humanoid({
-//     createdAt: new Date(),
-//     dimensions: {
-//       length: 2,
-//       width: 2,
-//       height: 2,
-//     },
-//     healthPoints: 15,
-//     name: 'Sir Mustachio',
-//     team: 'The Round Table',
-//     weapons: [
-//       'Giant Sword',
-//       'Shield',
-//     ],
-//     language: 'Common Tongue',
-//   });
+  const swordsman = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 2,
+      height: 2,
+    },
+    healthPoints: 15,
+    name: 'Sir Mustachio',
+    team: 'The Round Table',
+    weapons: [
+      'Giant Sword',
+      'Shield',
+    ],
+    language: 'Common Tongue',
+  });
 
-//   const archer = new Humanoid({
-//     createdAt: new Date(),
-//     dimensions: {
-//       length: 1,
-//       width: 2,
-//       height: 4,
-//     },
-//     healthPoints: 10,
-//     name: 'Lilith',
-//     team: 'Forest Kingdom',
-//     weapons: [
-//       'Bow',
-//       'Dagger',
-//     ],
-//     language: 'Elvish',
-//   });
+  const archer = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 1,
+      width: 2,
+      height: 4,
+    },
+    healthPoints: 10,
+    name: 'Lilith',
+    team: 'Forest Kingdom',
+    weapons: [
+      'Bow',
+      'Dagger',
+    ],
+    language: 'Elvish',
+  });
 
-//   console.log(mage.createdAt); // Today's date
-//   console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-//   console.log(swordsman.healthPoints); // 15
-//   console.log(mage.name); // Bruce
-//   console.log(swordsman.team); // The Round Table
-//   console.log(mage.weapons); // Staff of Shamalama
-//   console.log(archer.language); // Elvish
-//   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-//   console.log(mage.takeDamage()); // Bruce took damage.
-//   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+  console.log(mage.createdAt); // Today's date
+  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+  console.log(swordsman.healthPoints); // 15
+  console.log(mage.name); // Bruce
+  console.log(swordsman.team); // The Round Table
+  console.log(mage.weapons); // Staff of Shamalama
+  console.log(archer.language); // Elvish
+  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+  console.log(mage.takeDamage()); // Bruce took damage.
+  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
